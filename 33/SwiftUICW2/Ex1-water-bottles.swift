da7fa5fdfeecd410bddbdc8d5fb29395439beded
aishathaher <aisha.thaher@arch.ku.edu.kw>
2021-01-02 22:23:15 +0300

    3

---
+++ 33/SwiftUICW2/Ex1-water-bottles.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  SwiftUICW2
+//
+//  Created by Omar Alibrahim on 12/19/20.
+//  Copyright: Kuwait Codes 2020 code.kw
+import SwiftUI
+
+
+struct Exercise1: View {
+    @State var name = ""
+    @State var age = 0
+    var body: some View {
+        ZStack {
+            Image("charter")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+                Text("عهدة الماء 🚰")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+              
+                    
+                        VStack {
+                            TextField("Type your name here", text: $name)
+                            
+                            Stepper("كم بطلا من الماء ستشرب؟:",  value: $age, in: 1...122)
+                                .padding()
+                            Text("اتعهد انا \(name)")
+                            
+                            
+                            
+                        }
+//                        Stepper("كم بطلا من الماء تتعهد ان تشرب؟" ،value: "$WaterAmount")
+                        
+                        Spacer()
+                            .padding()
+                    }
+                }
+            }
+        }
+        
+        struct Exercise1_Previews: PreviewProvider {
+            static var previews: some View {
+                Exercise1()
+            }
+        }
---
+++ 33/SwiftUICW2/Ex2-currency-exchange.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  SwiftUICW2
+//
+//  Created by Omar Alibrahim on 12/19/20.
+//  Copyright: Kuwait Codes 2020 code.kw
+import SwiftUI
+
+struct Exercise2: View {
+    @State var dinars = ""
+    @State var dollars = ""
+    @State var pounds = ""
+    var body: some View {
+        
+        ZStack {
+            VStack{
+                Spacer()
+                Image("currency")
+                    .resizable()
+                    .scaledToFit()
+                    .ignoresSafeArea()
+                    .offset(y: 60.0)
+            }
+            ZStack {
+                VStack {
+                    Text("محول العملات العجيب!")
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    TextField("ضع القيمة بالدينار", text: $dinars)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .keyboardType(.decimalPad)
+
+                    VStack(spacing: 30){
+                        HStack(spacing: 40){
+                            Image("us")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50)
+                            
+                            let dollars = (Double(dinars) ?? 0) * 3.28
+                            Text("\(dollars)")
+                        }
+                        HStack(spacing: 40){
+                            Image("uk")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50)
+                            
+                            let pounds = (Double(dinars) ?? 0) * 2.46
+                            Text("\(pounds)")                        }
+                        HStack(spacing: 40){
+                            Image("eu")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50)
+                            let euros = (Double(dinars) ?? 0) * 2.70
+                            Text("\(euros)")
+                        }
+                    }.padding(.top, 50)
+                    Spacer()
+                }
+            }.padding()
+        }
+    }
+}
+
+struct Exercise2_Previews: PreviewProvider {
+    static var previews: some View {
+        Exercise2()
+    }
+}
